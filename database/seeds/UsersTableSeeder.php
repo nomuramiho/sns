ba2e58950f1c9cf36715cc265b3d74f38ba426b8
nomura_miho <n213y046@gmail.com>
2023-02-05 11:05:33 +0900

    シーダーに追加

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,6 +11,13 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        //下記コマンドを記述2/5
+        DB::table('posts')->insert([
+            ['post' => '1つ目の投稿です'],
+            ['post' => 'Laravelの投稿ページを作りました'],
+            ['post' => '投稿についてのCRUD一式を作っています'],
+            ['post' => 'MVCの役割を体験中です'],
+            ['post' => '初期レコードはこれで終わりです。']
+        ]);
     }
 }
