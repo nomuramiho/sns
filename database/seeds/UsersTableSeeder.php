c2a856c1b44f3bff509a94417c87a05eaa267a19
nomura_miho <n213y046@gmail.com>
2023-02-26 12:02:03 +0900

    シーダー再修正

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,14 +13,9 @@ class UsersTableSeeder extends Seeder
     {
         //下記コマンドを記述2/26
         DB::table('users')->insert([
-            ['users' => 'id'],
-            ['users' => 'username'],
-            ['users' => 'mail'],
-            ['users' => 'password'],
-            ['users' => 'bio'],
-            ['users' => 'image'],
-            ['users' => 'created_at'],
-            ['users' => 'updated_at']
+            ['username' => 'nomura',
+            'mail' => 'animal7cat@hotmail.co.jp',
+            'password' => 'animals7',]
         ]);
     }
 }
