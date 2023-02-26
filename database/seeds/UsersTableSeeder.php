60c156ffeade166948df1bd710555d6bcf3cefbf
nomura_miho <n213y046@gmail.com>
2023-02-26 11:24:47 +0900

    シーダー修正

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -11,11 +11,16 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        //下記コマンドを記述2/5
+        //下記コマンドを記述2/26
         DB::table('users')->insert([
-            ['post' => 'username'],
-            ['post' => 'mail'],
-            ['post' => 'password']
+            ['users' => 'id'],
+            ['users' => 'username'],
+            ['users' => 'mail'],
+            ['users' => 'password'],
+            ['users' => 'bio'],
+            ['users' => 'image'],
+            ['users' => 'created_at'],
+            ['users' => 'updated_at']
         ]);
     }
 }
